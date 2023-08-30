e4ff410d7498efc709836b184859026e470d68e2
Martín Repetto <martinleorepetto@gmail.com>
2023-08-30 16:14:48 -0300

    Version fallida map cluster

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import csvdata from "./data.csv";
 import { useState } from "react";
+import MarkerClusterGroup from "react-leaflet-cluster";
 const Papa = require("papaparse");
 
 function App() {
@@ -11,16 +12,24 @@ function App() {
     download: true,
     header: true,
     complete: function (results, file) {
-      let markerArray = []
+      let markerArray = [];
       jsonGeoData = results.data;
-      for (let i = 0; i < jsonGeoData.length-1; i++) {
+      for (let i = 0; i < jsonGeoData.length - 1; i++) {
         markerArray.push(
-          <Marker position={[jsonGeoData[i].Lat, jsonGeoData[i].Lng]}></Marker>
+          <Marker
+            key={jsonGeoData[i].key}
+            position={[jsonGeoData[i].Lat, jsonGeoData[i].Lng]}
+          >
+            <Popup>
+              <b>
+                {jsonGeoData[i].Title} - {jsonGeoData[i].Year}
+              </b>
+            </Popup>
+          </Marker>
         );
-
-        console.log(i);
+        // console.logs(i);
       }
-      setMarkers(markerArray)
+      setMarkers(markerArray);
     },
   });
 
@@ -36,7 +45,7 @@ function App() {
           attribution='<a href="https://www.ign.gob.ar/">Instituto Geográfico Nacional</a>'
           url="https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png"
         />
-        {markers}
+        <MarkerClusterGroup chunkedLoading>{markers}</MarkerClusterGroup>
       </MapContainer>
     </div>
   );
